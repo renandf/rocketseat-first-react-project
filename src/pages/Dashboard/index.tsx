f24200958b53e466ba99897a543cd90bb537d8c3
Renan Castro <renandf@gmail.com>
2020-09-02 08:38:41 +1000

    HTML/CSS done for Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,52 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
 
-import { Title } from "./styles";
+import logoImg from "../../assets/logo.svg";
+
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
-	return <Title>Explore repositories on Github</Title>;
+	return (
+		<>
+			<img src={logoImg} alt="Github Explorer" />
+			<Title>Explore repositories on Github</Title>
+
+			<Form>
+				<input placeholder="Search for a repository..." />
+				<button type="submit">Search</button>
+			</Form>
+
+			<Repositories>
+				<a href="test">
+					<img src="https://placeimg.com/200/200/people" alt="Person" />
+					<div>
+						<strong>rocketseat/unform</strong>
+						<p>Testing description paragraph</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+				<a href="test">
+					<img src="https://placeimg.com/200/200/people" alt="Person" />
+					<div>
+						<strong>rocketseat/unform</strong>
+						<p>Testing description paragraph</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+				<a href="test">
+					<img src="https://placeimg.com/200/200/people" alt="Person" />
+					<div>
+						<strong>rocketseat/unform</strong>
+						<p>Testing description paragraph</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+			</Repositories>
+		</>
+	);
 };
 
 export default Dashboard;
