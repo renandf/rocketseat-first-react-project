59c174f3cad1cfa4beed39e1a236e9a63e604640
Renan Castro <renandf@gmail.com>
2020-09-02 07:41:57 +1000

    initial commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Title } from "./styles";
+
+const Dashboard: React.FC = () => {
+	return <Title>Explore repositories on Github</Title>;
+};
+
+export default Dashboard;
